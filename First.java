a4a339e191d5098a3bde2e951bb9de52d61ff1da
Aman <rajputaman8840@gmail.com>
2022-08-06 14:56:28 +0530

    First commit

---
+++ First.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class First{
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int radius =sc.nextInt();
+        
+        double area = radius*radius*3.14;
+        System.out.println(area);
+    }
+}
---
+++ aman.java
@@ -0,0 +1,16 @@
+class aman {
+    public static void main(String[] args) {
+        System.out.println("AMAN");
+        // Variables
+String name = "Aman";
+int age = 30;
+String neighbour = "Akku";
+String friend = neighbour;
+
+        System.out.print(age);
+        System.out.print(age);
+        System.out.print(friend);
+
+        
+    }
+}
